44291587258c9cd8b5f7b190b710d77ffd76524a
seung <gam4it@gmail.com>
2016-08-03 12:00:33 +0000

    modified to generate parameter + id

--- PushId.php
+++ PushId.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Fancy ID generator that creates 20-character string identifiers with the following properties:
  *
@@ -18,14 +17,12 @@ class PushId
      * @var string
      */
     const PUSH_CHARS = '-0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz';
-
     /**
      * Timestamp of last push, used to prevent local collisions if you push twice in one ms.
      *
      * @var int
      */
     private static $lastPushTime = 0;
-
     /**
      * We generate 72-bits of randomness which get turned into 12 characters and appended to the
      * timestamp to prevent collisions with other clients.  We store the last characters we
@@ -35,30 +32,25 @@ class PushId
      * @var array
      */
     private static $lastRandChars = [];
-
     /**
      * @return string
      */
-    public static function generate()
+    public static function generate($pre = '')
     {
         $now = (int) microtime(true) * 1000;
         $isDuplicateTime = ($now === static::$lastPushTime);
         static::$lastPushTime = $now;
-
-        $timeStampChars = new SplFixedArray(8);
+        $timeStampChars = new \SplFixedArray(8);
         for ($i = 7; $i >= 0; $i--) {
             $timeStampChars[$i] = substr(self::PUSH_CHARS, $now % 64, 1);
             // NOTE: Can't use << here because javascript will convert to int and lose the upper bits.
             $now = (int) floor($now / 64);
         }
-
         static::assert($now === 0, 'We should have converted the entire timestamp.');
-
         $id = implode('', $timeStampChars->toArray());
-
         if (!$isDuplicateTime) {
             for ($i = 0; $i < 12; $i++) {
-                $lastRandChars[$i] = floor(rand(0, 64));
+                static::$lastRandChars[$i] = (int) floor(rand(0, 63));
             }
         } else {
             // If the timestamp hasn't changed since last push, use the same random number, except incremented by 1.
@@ -68,22 +60,20 @@ class PushId
             static::$lastRandChars[$i]++;
         }
         for ($i = 0; $i < 12; $i++) {
-            $id .= substr(self::PUSH_CHARS, $lastRandChars[$i], 1);
+            $id .= substr(self::PUSH_CHARS, static::$lastRandChars[$i], 1);
         }
-
         static::assert(strlen($id) === 20, 'Length should be 20.');
-
-        return $id;
+        return $pre . $id;
     }
-
     /**
-     * @param bool   $condition
+     * @param $condition
      * @param string $message
+     * @throws \RuntimeException
      */
     private static function assert($condition, $message = '')
     {
         if ($condition !== true) {
-            throw new RuntimeException($message);
+            throw new \RuntimeException($message);
         }
     }
 }
